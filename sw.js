3aab208b7810dc22289202d018d214a0e4666ef3
Brett Holcomb <ready.set.get.brett@gmail.com>
2024-08-20 16:07:14 -0700

    Updates

---
+++ sw.js
@@ -0,0 +1,26 @@
+var GHPATH = "/feathers";
+var APP_PREFIX = "feathers_";
+
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…).
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = "version_00";
+
+// The files to make available for offline use. make sure to add
+// others to this list
+var URLS = [
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+
+  `${GHPATH}/assets/index-BfDuDVHZ.js`,
+  `${GHPATH}/assets/index-CHpxEex4.css`,
+  `${GHPATH}/mushroom.png`,
+  `${GHPATH}/cloud.png`,
+  `${GHPATH}/coin.png`,
+  `${GHPATH}/drop.png`,
+  `${GHPATH}/feather.png`,
+  `${GHPATH}/feather.svg`,
+  `${GHPATH}/glow.png`,
+  `${GHPATH}/moon.png`,
+];
