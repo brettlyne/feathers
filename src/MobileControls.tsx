6cec5d8f24a1c4d68e1fc83031025f84922c333a
Brett Holcomb <ready.set.get.brett@gmail.com>
2024-08-20 17:45:12 -0700

    add BottomNavigation

--- src/MobileControls.tsx
+++ src/MobileControls.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+
 import Button from "@mui/material/Button";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import VideocamIcon from "@mui/icons-material/Videocam";
+import AppsIcon from "@mui/icons-material/Apps";
+import HexagonIcon from "@mui/icons-material/Hexagon";
+import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
+
 import { AnimationType } from "./util/animations";
 import {
   VisualizationState,
@@ -12,10 +20,23 @@ interface ControlsProps {
 }
 
 const Controls: React.FC<ControlsProps> = ({ state, updateState }) => {
+  const [value, setValue] = React.useState(0);
+
   return (
     <div className="mobile-controls">
       <h1>hello</h1>
-      <Button variant="contained">Hello world</Button>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Scene" icon={<VideocamIcon />} />
+        <BottomNavigationAction label="Field" icon={<AppsIcon />} />
+        <BottomNavigationAction label="Shape" icon={<HexagonIcon />} />
+        <BottomNavigationAction label="Animation" icon={<AutoAwesomeIcon />} />
+      </BottomNavigation>
     </div>
   );
 };
