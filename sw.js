71e5ccf9e7e77157b6162504db4d8d3935f59197
Brett Holcomb <ready.set.get.brett@gmail.com>
2024-08-21 03:44:03 -0700

    Updates

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ var APP_PREFIX = "feathers_";
 // you need to change this version (version_01, version_02…).
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = "version_01";
+var VERSION = "version_02";
 
 // The files to make available for offline use. make sure to add
 // others to this list
@@ -13,8 +13,8 @@ var URLS = [
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
 
-  `${GHPATH}/assets/index-FFoy6OyJ.js`,
-  `${GHPATH}/assets/index-FFoy6OyJ.js`,
+  `${GHPATH}/assets/index-BzFaeIEV.js`,
+  `${GHPATH}/assets/index-D8NqQ6V1.css`,
   `${GHPATH}/mushroom.png`,
   `${GHPATH}/cloud.png`,
   `${GHPATH}/coin.png`,
@@ -23,4 +23,8 @@ var URLS = [
   `${GHPATH}/feather.svg`,
   `${GHPATH}/glow.png`,
   `${GHPATH}/moon.png`,
+  `${GHPATH}/presets/preset-1.png`,
+  `${GHPATH}/presets/preset-2.png`,
+  `${GHPATH}/presets/preset-3.png`,
+  `${GHPATH}/presets/preset-4.png`,
 ];
