034623dd0563a93e623ab96be5442d99846164f4
Brett Holcomb <ready.set.get.brett@gmail.com>
2024-09-04 17:32:44 -0700

    Update MobileApp.tsx

--- src/MobileApp.tsx
+++ src/MobileApp.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { ArcballControls, Stats } from "@react-three/drei";
+// import { ArcballControls, Stats } from "@react-three/drei";
+import { ArcballControls } from "@react-three/drei";
 import * as THREE from "three";
 import HexagonParticles from "./HexagonParticles";
 import MobileControls from "./MobileControls";
@@ -120,7 +121,7 @@ const App: React.FC = () => {
         updateState={updateVState}
         setVState={setVState}
       />
-      <Stats />
+      {/* <Stats /> */}
     </div>
   );
 };
