e9c22647938557433817424bc942734b2f94fc37
Brett Holcomb <ready.set.get.brett@gmail.com>
2024-09-07 22:22:58 -0700

    use vite-plugin-pwa instead of manual deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "Feathers",
+        short_name: "Feathers",
+        description: "Feathers Visualization as a Progressive Web App",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/feathers/featherIcon600.png",
+            type: "image/png",
+            sizes: "600x600",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/feathers/",
 });
